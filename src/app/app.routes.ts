2720679a455000432fd9338016e8c97c9e195066
Rémi <remi.huguenot@diiage.org>
2024-03-06 15:03:48 +0100

    Setup environment variables and API services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListEventsComponent } from './list-events/list-events.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { path: 'events', component: ListEventsComponent },
+];
